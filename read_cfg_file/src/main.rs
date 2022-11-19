8838bf5866a3a4496df83feaa2ac6f1ff44ce8ac
xianghengliang <hengliangxiang@qq.com>
2022-11-19 10:55:43 +0800

    add toml file parse

--- read_cfg_file/src/main.rs
+++ read_cfg_file/src/main.rs
@@ -1,12 +1,21 @@
 use std::fs::File;
 use std::io::Read;
+// use toml::value::*;
 use serde::{Deserialize, Serialize};
 
+// todo: 可扩展性， 使用enum 或者 struct 来实现 json和toml文件的解析过程
+// json文件解析
 #[derive(Serialize, Deserialize, Debug)]
 struct Args {
     development: Development,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+struct Develop {
+    address: String,
+    port: String,
+    workers: u64,
+}
 #[derive(Serialize, Deserialize, Debug)]
 struct Development {
     address: String,
@@ -21,11 +30,25 @@ struct Database {
     pool: u8,
 }
 
+// toml文件解析
+#[derive(Serialize, Deserialize, Debug)]
+struct TomlArgs { 
+    development: Develop,
+    database: Database, // todo: #development.database, 需要名字的转换？参考letmeknow
+}
+
 fn main() {
     let mut json_file = File::open("./cfg_files/test1.json").unwrap();
     let mut buff = String::new();
-    json_file.read_to_string(&mut buff).unwrap();
-
+    json_file.read_to_string(&mut buff).unwrap(); // todo:增加错误处理
+    println!("1.1 read json file to buff: {:?}", buff);
     let readed_json: Args = serde_json::from_str(&buff).unwrap();
-    println!("read json file: {:?}", readed_json);
+    println!("1.2 read json file: {:?}", readed_json);
+
+    let mut toml_file = File::open("./cfg_files/test2.toml").unwrap();
+    let mut toml_buff = String::new();
+    toml_file.read_to_string(&mut toml_buff).unwrap();
+    println!("2.1 read toml file to buff: {:?}", toml_buff);
+    let readed_toml: TomlArgs = toml::from_str(&toml_buff).unwrap();
+    println!("2.2 read toml file: {:?}", readed_toml); // toml_buff
 }
